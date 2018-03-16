64418b5c1b881d8ac7e22f0531dfb8d15e2f8fb0
jd0824um <jd0824um@minneapolis.edu>
2018-03-16 01:19:09 -0500

    Finished the add form. Added additional validation checks. Added ability to add a picture with an item. TODO: Issue when adding a item without a picture then adding one with a picture. Both have same picture

--- Inventory Management/Add Product.Designer.cs
+++ Inventory Management/Add Product.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Inventory_Management
 {
-    partial class Add_Product
+    partial class formAddProduct
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboType = new System.Windows.Forms.ComboBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtDistributor = new System.Windows.Forms.TextBox();
+            this.lblDistributor = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBoxAddProduct = new System.Windows.Forms.PictureBox();
+            this.openFileDialogPicture = new System.Windows.Forms.OpenFileDialog();
+            this.btnImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAddProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboType
+            // 
+            this.comboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboType.FormattingEnabled = true;
+            this.comboType.Location = new System.Drawing.Point(111, 40);
+            this.comboType.Name = "comboType";
+            this.comboType.Size = new System.Drawing.Size(100, 21);
+            this.comboType.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(16, 17);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(78, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Product Name:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(16, 43);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(74, 13);
+            this.lblType.TabIndex = 2;
+            this.lblType.Text = "Product Type:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(111, 14);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(111, 67);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(56, 70);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(34, 13);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "Price:";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(41, 96);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(49, 13);
+            this.lblQuantity.TabIndex = 7;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(111, 93);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 8;
+            // 
+            // txtDistributor
+            // 
+            this.txtDistributor.Location = new System.Drawing.Point(111, 119);
+            this.txtDistributor.Name = "txtDistributor";
+            this.txtDistributor.Size = new System.Drawing.Size(100, 20);
+            this.txtDistributor.TabIndex = 9;
+            // 
+            // lblDistributor
+            // 
+            this.lblDistributor.AutoSize = true;
+            this.lblDistributor.Location = new System.Drawing.Point(33, 122);
+            this.lblDistributor.Name = "lblDistributor";
+            this.lblDistributor.Size = new System.Drawing.Size(57, 13);
+            this.lblDistributor.TabIndex = 10;
+            this.lblDistributor.Text = "Distributor:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(237, 122);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(50, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(299, 122);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(50, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBoxAddProduct
+            // 
+            this.pictureBoxAddProduct.Location = new System.Drawing.Point(237, 4);
+            this.pictureBoxAddProduct.Name = "pictureBoxAddProduct";
+            this.pictureBoxAddProduct.Size = new System.Drawing.Size(112, 79);
+            this.pictureBoxAddProduct.TabIndex = 13;
+            this.pictureBoxAddProduct.TabStop = false;
+            // 
+            // openFileDialogPicture
+            // 
+            this.openFileDialogPicture.FileName = "openFileDialogPicture";
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(253, 90);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(75, 23);
+            this.btnImage.TabIndex = 14;
+            this.btnImage.Text = "Add Image";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
+            // 
+            // formAddProduct
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Add_Product";
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(361, 154);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.pictureBoxAddProduct);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblDistributor);
+            this.Controls.Add(this.txtDistributor);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.comboType);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formAddProduct";
+            this.Text = "Add Product";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAddProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboType;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtDistributor;
+        private System.Windows.Forms.Label lblDistributor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBoxAddProduct;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPicture;
+        private System.Windows.Forms.Button btnImage;
     }
 }
